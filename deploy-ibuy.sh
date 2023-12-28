e40790e69ffe29e6d258dfcba87e8a4b5fd26370
Azdad Anass <a.azdad@3gcom-int.com>
2023-12-28 19:36:50 +0100

    change docker repos

--- deploy-ibuy.sh
+++ deploy-ibuy.sh
@@ -3,6 +3,6 @@ mkdir -p ~/log
 
 docker stop ibuy-orange
 docker rm ibuy-orange
-docker image rm 41.248.252.127:5000/ibuy-orange:latest
-docker pull 41.248.252.127:5000/ibuy-orange:latest
-docker run -d --name ibuy-orange -p 8086:8080 -v ~/files/:/usr/local/tomcat/webapps/ROOT/files/ -e LANG="fr_FR.UTF-8" -e LC_ALL="fr_FR.UTF-8" -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro 41.248.252.127:5000/ibuy-orange:latest
+docker image rm azdadanass/ibuy-orange:latest
+docker pull azdadanass/ibuy-orange:latest
+docker run -d --name ibuy-orange -p 8086:8080 -v ~/files/:/usr/local/tomcat/webapps/ROOT/files/ -e LANG="fr_FR.UTF-8" -e LC_ALL="fr_FR.UTF-8" -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro azdadanass/ibuy-orange:latest
--- deploy-sdm.sh
+++ deploy-sdm.sh
@@ -3,6 +3,6 @@ mkdir -p ~/log
 
 docker stop sdm-orange
 docker rm sdm-orange
-docker image rm 41.248.252.127:5000/sdm-orange:latest
-docker pull 41.248.252.127:5000/sdm-orange:latest
-docker run -d --name sdm-orange -p 8082:8080 -v ~/files/:/usr/local/tomcat/webapps/ROOT/files/  -e LANG="fr_FR.UTF-8" -e LC_ALL="fr_FR.UTF-8" -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro 41.248.252.127:5000/sdm-orange:latest
+docker image rm azdadanass/sdm-orange:latest
+docker pull azdadanass/sdm-orange:latest
+docker run -d --name sdm-orange -p 8082:8080 -v ~/files/:/usr/local/tomcat/webapps/ROOT/files/  -e LANG="fr_FR.UTF-8" -e LC_ALL="fr_FR.UTF-8" -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro azdadanass/sdm-orange:latest
